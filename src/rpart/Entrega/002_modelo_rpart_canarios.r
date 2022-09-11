8839056cded73bbd93d2dec8840f3a0a94b468b8
gasper-az <acevedo.zain.gaspar@gmail.com>
2022-09-11 19:32:57 -0300

    v1.0.5 ===> finalizando exp

--- src/rpart/Entrega/002_modelo_rpart_canarios.r
+++ src/rpart/Entrega/002_modelo_rpart_canarios.r
@@ -31,6 +31,93 @@ dataset[ foto_mes==202101,
 #-------------------- FEATURE ENGINEERING --------------------#
 #-------------------------------------------------------------#
 
+# Feature Engineering del tipo AX + BY, aplicado a columnas asociadas a la
+# tarjeta del cliente (Master)
+dataset[, master_fe_suma_all := 
+          # Master_mfinanciacion_limite +
+          Master_msaldototal +
+          Master_msaldopesos + Master_msaldodolares +
+          Master_mconsumospesos + Master_mconsumosdolares +
+          Master_mlimitecompra +
+          Master_madelantopesos +
+          Master_madelantodolares + 
+          Master_mpagado +
+          Master_mpagospesos +
+          Master_mpagosdolares + Master_mconsumototal + Master_mpagominimo
+]
+
+# Feature Engineering del tipo AX + BY, aplicado a columnas asociadas a la
+# tarjeta del cliente (Visa)
+dataset[, visa_fe_suma_all := Visa_mfinanciacion_limite +
+          # Visa_msaldototal +
+          Visa_msaldopesos +
+          Visa_msaldodolares + Visa_mconsumospesos +
+          Visa_mconsumosdolares +
+          Visa_mlimitecompra +
+          Visa_madelantopesos +
+          Visa_madelantodolares +
+          Visa_mpagado +
+          Visa_mpagospesos +
+          Visa_mpagosdolares + Visa_mconsumototal
+        + Visa_mpagominimo
+]
+
+# Feature Engineering del tipo AX + BY, aplicado a columnas asociadas a las
+# tarjetas del cliente (Master + Visa)
+dataset[, tarjetas_fe_suma_all := master_fe_suma_all + visa_fe_suma_all]
+
+
+# Feature Engineering del tipo AX + BY, aplicado a todas las columnas en pesos
+# salvo las tarjetas
+dataset[, pesos_fe_suma_menos_tarjetas := 
+          # mrentabilidad +
+          # mrentabilidad_annual +
+          mcomisiones +
+          # mactivos_margen +
+          # mpasivos_margen +
+          mcuenta_corriente_adicional + 
+          # mcuenta_corriente +
+          # mcaja_ahorro +
+          mcaja_ahorro_adicional +
+          # mcaja_ahorro_dolares + 
+          mcuentas_saldo +
+          mautoservicio + mtarjeta_visa_consumo +
+          mtarjeta_master_consumo +
+          mprestamos_personales + mprestamos_prendarios +
+          mprestamos_hipotecarios + mplazo_fijo_dolares + mplazo_fijo_pesos +
+          minversion1_pesos +
+          minversion1_dolares + minversion2 + 
+          mpayroll +
+          mpayroll2 + 
+          mcuenta_debitos_automaticos +
+          mttarjeta_master_debitos_automaticos + mpagodeservicios +
+          mpagomiscuentas +
+          mcajeros_propios_descuentos +
+          mtarjeta_visa_descuentos + mtarjeta_master_descuentos +
+          # mcomisiones_mantenimiento +
+          mcomisiones_otras +
+          mforex_buy +
+          mforex_sell +
+          mtransferencias_recibidas +
+          mtransferencias_emitidas +
+          mextraccion_autoservicio +
+          mcheques_depositados + mcheques_emitidos +
+          mcheques_depositados_rechazados + mcheques_emitidos_rechazados +
+          matm + matm_other
+]
+
+# Feature Engineering del tipo AX + BY, aplicado a todas las columnas en pesos
+dataset[, pesos_fe_suma_all :=
+          pesos_fe_suma_menos_tarjetas +
+          tarjetas_fe_suma_all
+]
+
+# Feature Engineering del tipo A/B, aplicado a variables más importantes para el
+# modelo, pero que SI estén en el gráfico, y performen mejor que canarios
+dataset[, cociente_fe_01 := ctrx_quarter/mcuentas_saldo]
+dataset[, cociente_fe_02 := ctrx_quarter/mcomisiones]
+dataset[, cociente_fe_03 := mcuentas_saldo/mcomisiones]
+
 #-------------------------------------------------------------------#
 #------------------- AGREGO N VARIABLES CANARIOS -------------------#
 #-------------------------------------------------------------------#
@@ -67,8 +154,8 @@ variables.drifting <- c(
 
 variables.sacar <- c(
   "clase_ternaria"
-  # ,
-  # variables.drifting
+  ,
+  variables.drifting
 )
 
 #--------------------------------------------------------------------------------------------#
@@ -76,8 +163,32 @@ variables.sacar <- c(
 #--------------------------------------------------------------------------------------------#
 
 variables.sacar <- c(
-  variables.sacar
+  variables.sacar,
+  "cliente_edad",
+  "numero_de_cliente",
+  "Master_mfinanciacion_limite",
+  "mpasivos_margen",
+  "ccajas_extracciones",
+  "Visa_msaldototal",
+  "mcuenta_corriente",
+  "ctarjeta_master_debitos_automaticos",
+  "ccallcenter_transacciones",
+  "ctarjeta_master_transacciones",
+  "mttarjeta_visa_debitos_automaticos",
+  "mactivos_margen",
+  "Master_fechaalta",
+  "Master_Fvencimiento",
+  "mcaja_ahorro_dolares",
+  "mrentabilidad",
+  "cliente_antiguedad",
+  "mcaja_ahorro",
+  "Visa_status",
+  "Visa_Fvencimiento",
+  "Visa_fechaalta",
+  "mrentabilidad_annual",
+  "mcomisiones_mantenimiento"
 )
+
 
 #-----------------------------------------------------------------#
 #-------------------- Creo fórmula del modelo --------------------#
@@ -93,25 +204,35 @@ for (variable in variables.sacar) {
 #-------------------- Creo el modelo --------------------#
 #--------------------------------------------------------#
 
-modelo.original  <- rpart(
-  formula  = formula.modelo,
-  data     = dtrain,
-  xval     = 0,
-  cp       = -1,
-  minsplit = 2,
-  minbucket= 1,
-  maxdepth = 10
-)
+# modelo.original  <- rpart(
+#   formula  = formula.modelo,
+#   data     = dtrain,
+#   xval     = 0,
+#   cp       = -1,
+#   minsplit = 2,
+#   minbucket= 1,
+#   maxdepth = 10
+# )
 
 # modelo.original  <- rpart(
 #   formula  = formula.modelo,
 #   data     = dtrain,
 #   xval     = 0,
-#   cp       = -0.816404835,
-#   minsplit = 1629,
-#   minbucket= 814,
-#   maxdepth = 7
+#   cp       = -0.56159657,
+#   minsplit = 1485,
+#   minbucket= 129,
+#   maxdepth = 10
 # )
+
+modelo.original  <- rpart(
+  formula  = formula.modelo,
+  data     = dtrain,
+  xval     = 0,
+  cp       = -0.56159657,
+  minsplit = 1485,
+  minbucket= 129,
+  maxdepth = 8
+)
 
 # modelo.original  <- rpart(
 #   formula  = formula.modelo,
