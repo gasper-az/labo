c3e5831b6b4c9f112ef4f0ac7afb5288efc411e0
gasper-az <acevedo.zain.gaspar@gmail.com>
2022-09-11 23:35:15 -0300

    Entrega final

--- src/rpart/Entrega/modelo_rpart_binario.r
+++ src/rpart/Entrega/modelo_rpart_binario.r
@@ -160,15 +160,16 @@ setorder(dfinal, -prob_SI, azar)
 
 dir.create( "./exp/" )
 dir.create( "./exp/HT0909" )
-dir.create( "./exp/HT0909/v1.0" )
+dir.create( "./exp/HT0909/vFinal" )
 
-for(corte in c(7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000)) {
+# for(corte in c(7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000)) {
+for(corte in c(9500)) {
   dfinal[ , Predicted := 0L ]
   dfinal[ 1:corte , Predicted := 1L ]
   
   fwrite(
     dfinal[, list(numero_de_cliente, Predicted)], #solo los campos para Kaggle
-    file= paste0("./exp/HT0909/v1.0/KA4120_005_", corte, ".csv"),
+    file= paste0("./exp/HT0909/vFinal/KA4120_005_", corte, ".csv"),
     sep=  ","
   )
 }
@@ -177,6 +178,6 @@ for(corte in c(7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000)) {
 #-------------------- Guardo el modelo como PDF --------------------#
 #-------------------------------------------------------------------#
 
-pdf(file = "./exp/HT0909/v1.0/rpart.pdf", width=28, height=4)
+pdf(file = "./exp/HT0909/vFinal/rpart.pdf", width=28, height=4)
 prp(modelo, extra=101, digits=5, branch=1, type=4, varlen=0, faclen=0)
 dev.off()
