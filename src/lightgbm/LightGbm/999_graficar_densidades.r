b2f0d6028545bd54eb95eb1a481d480621c9f324
gasper-az <acevedo.zain.gaspar@gmail.com>
2022-09-28 02:15:08 -0300

    Mejoro script de análisis de densidades

--- src/lightgbm/LightGbm/999_graficar_densidades.r
+++ src/lightgbm/LightGbm/999_graficar_densidades.r
@@ -8,21 +8,30 @@ require("lightgbm")
 
 #------------------------------------------------------------------------------
 
-graficar_campo  <- function( campo, campo_clase, valores_clase )
+graficar_campo  <- function( campo, campo_clase, valores_clase, ini, fin, considerar.campo = TRUE )
 {
 
   #quito de grafico las colas del 5% de las densidades
-  qA  <- quantile(  dataset[ foto_mes==202101 , get(campo) ] , prob= c(0.05, 0.95), na.rm=TRUE )
-  qB  <- quantile(  dataset[ foto_mes==202103 , get(campo) ] , prob= c(0.05, 0.95), na.rm=TRUE )
+  qA  <- quantile(  dataset[ foto_mes==ini , get(campo) ] , prob= c(0.05, 0.95), na.rm=TRUE )
+  qB  <- quantile(  dataset[ foto_mes==fin , get(campo) ] , prob= c(0.05, 0.95), na.rm=TRUE )
 
   xxmin  <- pmin( qA[[1]], qB[[1]] )
   xxmax  <- pmax( qA[[2]], qB[[2]] )
 
-  densidad_A  <- density( dataset[ foto_mes==202101 & get(campo_clase) %in% valores_clase, get(campo) ],
-                          kernel="gaussian", na.rm=TRUE )
-
-  densidad_B  <- density( dataset[ foto_mes==202103 & get(campo_clase) %in% valores_clase, get(campo) ],
-                          kernel="gaussian", na.rm=TRUE )
+  if (considerar.campo) {
+    densidad_A  <- density( dataset[ foto_mes==ini & get(campo_clase) %in% valores_clase, get(campo) ],
+                            kernel="gaussian", na.rm=TRUE )
+    
+    densidad_B  <- density( dataset[ foto_mes==fin & get(campo_clase) %in% valores_clase, get(campo) ],
+                            kernel="gaussian", na.rm=TRUE )
+  } else {
+    densidad_A  <- density( dataset[ foto_mes==ini, get(campo) ],
+                            kernel="gaussian", na.rm=TRUE )
+    
+    densidad_B  <- density( dataset[ foto_mes==fin, get(campo) ],
+                            kernel="gaussian", na.rm=TRUE )
+  }
+  
 
   plot( densidad_A,
         col="blue",
@@ -45,10 +54,14 @@ setwd("C:\\uba\\dmeyf")
 #cargo el dataset donde voy a entrenar
 dataset  <- fread("./datasets/competencia2_2022.csv.gz")
 
-dataset  <- dataset[  foto_mes %in% c( 202101, 202103 ) ]
+mes.ini <- 202101
+mes.fin <- 202103
+considerar.campo <- TRUE
+
+dataset  <- dataset[  foto_mes %in% c( mes.ini, mes.fin ) ]
 
 #creo la clase_binaria SI={ BAJA+1, BAJA+2 }    NO={ CONTINUA }
-dataset[ foto_mes==202101, 
+dataset[ foto_mes==mes.ini, 
          clase_binaria :=  ifelse( clase_ternaria=="CONTINUA", "NO", "SI" ) ]
 
 
@@ -57,26 +70,16 @@ dataset[ , clase01 := ifelse( clase_ternaria %in%  c("BAJA+2","BAJA+1"), 1L, 0L)
 
 PARAM <- list()
 PARAM$input$dataset       <- "./datasets/competencia2_2022.csv.gz"
-PARAM$input$training      <- c( 202101 )
+PARAM$input$training      <- c( mes.ini )
 
 PARAM$finalmodel$max_bin           <- 31
-PARAM$finalmodel$learning_rate     <- 0.005201496  #0.0142501265
-PARAM$finalmodel$num_iterations    <- 1444  #615
-PARAM$finalmodel$num_leaves        <- 1024  #784
-PARAM$finalmodel$min_data_in_leaf  <- 1968  #5628
-PARAM$finalmodel$feature_fraction  <- 0.750240992  #0.8382482539
+PARAM$finalmodel$learning_rate     <- 0.005201496
+PARAM$finalmodel$num_iterations    <- 1444
+PARAM$finalmodel$num_leaves        <- 1024
+PARAM$finalmodel$min_data_in_leaf  <- 1968
+PARAM$finalmodel$feature_fraction  <- 0.750240992
 PARAM$finalmodel$semilla           <- 763369
 PARAM$finalmodel$bagging_fraction  <-	0.66574846403701
-
-# Entreno el modelo
-# utilizo los mejores hiperparametros encontrados en una Bayesian Optimizationcon 5-fold Cross Validation
-# modelo  <- rpart(formula=   "clase_binaria ~ . -clase_ternaria",
-#                  data=      dataset[ foto_mes==202101 ],  #los datos donde voy a entrenar
-#                  xval=         0,
-#                  cp=           -0.69,
-#                  minsplit=    870,
-#                  minbucket=     9,
-#                  maxdepth=      9)
 
 #establezco donde entreno
 dataset[ , train  := 0L ]
@@ -100,15 +103,12 @@ modelo  <- lgb.train( data= dtrain,
 
 campos_modelo  <- names( modelo$variable.importance )
 campos_buenos  <- c( campos_modelo,  setdiff( colnames(dataset), campos_modelo ) )
-campos_buenos  <-  setdiff(  campos_buenos,  c( "foto_mes","clase_ternaria","clase_binaria" ) )
-# campos_buenos  <- setdiff( colnames(dataset), c("clase_ternaria","clase01") )
+campos_buenos  <- setdiff(  campos_buenos,  c( "foto_mes","clase_ternaria","clase_binaria" ) )
 
 
 dir.create( "./exp/",  showWarnings = FALSE ) 
 dir.create( "./exp/DR6135/", showWarnings = FALSE )
 setwd("./exp/DR6135/")
-
-
 
 pdf("densidades_01_03_lgbm.pdf")
 
@@ -116,11 +116,11 @@ for( campo in  campos_buenos )
 {
   cat( campo, "  " )
   
-  graficar_campo( campo, "clase_ternaria", c( "BAJA+1", "BAJA+2", "CONTINUA" ) )
-  graficar_campo( campo, "clase_ternaria", c( "BAJA+1", "BAJA+2" ) )
-  graficar_campo( campo, "clase_ternaria", c( "BAJA+2" ) )
-  graficar_campo( campo, "clase_ternaria", c( "BAJA+1" ) )
-  graficar_campo( campo, "clase_ternaria", c( "CONTINUA" ) )
+  graficar_campo( campo, "clase_ternaria", c( "BAJA+1", "BAJA+2", "CONTINUA" ), mes.ini, mes.fin, considerar.campo )
+  graficar_campo( campo, "clase_ternaria", c( "BAJA+1", "BAJA+2" ), mes.ini, mes.fin, considerar.campo )
+  graficar_campo( campo, "clase_ternaria", c( "BAJA+2" ), mes.ini, mes.fin, considerar.campo )
+  graficar_campo( campo, "clase_ternaria", c( "BAJA+1" ), mes.ini, mes.fin, considerar.campo )
+  graficar_campo( campo, "clase_ternaria", c( "CONTINUA" ), mes.ini, mes.fin, considerar.campo )
 }
 
 dev.off()
